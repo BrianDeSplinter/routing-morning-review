178e5505adb428c1c12613afef104c3e41192766
BrianDeSplinter <bdesplinter34@gmail.com>
2020-06-01 16:33:39 -0600

    Finished

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+export default function Header(){
+    return(
+        <div>
+            <h2>Nice To Meet Me</h2>
+            <Link to= '/'> {`<Link Back to the Conference Room`} </Link>
+        </div>
+    )
+}
